abd2e2943823d0cf10622d940bacd0f2423406aa
unknown <himlee@gmail.com>
2024-01-23 23:11:37 +0000

    Update VulnerabilityController and views

--- resources/views/vulnerabilities.blade.php
+++ resources/views/vulnerabilities.blade.php
@@ -1,25 +1,31 @@
+<div class="grid grid-cols-4 bg-yellow-500 min-h-screen p-4">
 
+    @foreach($vulnerabilities as $vulnerability)
 
-@if (Route::has('login'))
-    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-        @auth
-            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-        @else
-            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+        <div class="mb-4">
+            <h2 class="text-white bg-blue-500 p-2">Vulnerability {{ $vulnerability->id }}</h2>
 
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-            @endif
-        @endauth
-    </div>
-@endif
+            <table class="w-full bg-green p-2">
+                <tr>
+                    <th class="border">Vulnerability ID</th>
+                    <th class="border">Vulnerability Name</th>
+                    <th class="border">Severity</th>
+                    <td class="border">{{"            "}}</td>
+                    <th class="border">Select</th>
+                </tr>
 
-<div class="grid grid-cols-4">
-    @foreach($vulnerbilities as $vulnerbility)
-        {{$vulnerbility}}
+                <tr>
+                    <td class="border">{{ $vulnerability->id }}</td>
+                    <td class="border">{{ $vulnerability->name }}</td>
+                    <td class="border">{{ $vulnerability->severity }}</td>
+                    <td class="border">{{"       "}}</td>
+                    <td class="border"> <button value="{{ $vulnerability->id }}" class="select-product">Select</button> </td>
+                </tr>
+            </table>
+        </div>
+
     @endforeach
-</div>
-
-<div >
 
 </div>
+
+
