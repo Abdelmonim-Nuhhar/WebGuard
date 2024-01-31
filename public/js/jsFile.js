73d487aee77574c44e1d0b41330a1a8d9826e07c
unknown <himlee@gmail.com>
2024-01-31 11:55:08 +0000

    Revamp Welcome Page UI:
    - Enhance visual appeal with updated styles.
    - Implement 'View more' and 'View less' functionality.
    - Improve registration call-to-action and content.
    - Refactor JavaScript into an external file for better maintainability.

---
+++ public/js/jsFile.js
@@ -0,0 +1,29 @@
+
+        function toggleDescription() {
+            var expandedDescription = document.getElementById('expandedDescription');
+            var learnMoreLink = document.getElementById('learnMoreLink');
+            var viewLessLink = document.getElementById('viewLessLink');
+
+            if (expandedDescription.classList.contains('hidden')) {
+                // Show description and change link text
+                expandedDescription.classList.remove('hidden');
+                learnMoreLink.classList.add('hidden'); // Hide "Learn More" link
+                viewLessLink.classList.remove('hidden'); // Show "View Less" link
+
+                // Scroll to the end of the page
+                window.scrollTo({
+                    top: document.body.scrollHeight,
+                    behavior: 'smooth'
+                });
+            } else {
+                // Hide description and change link text
+                expandedDescription.classList.add('hidden');
+                learnMoreLink.classList.remove('hidden'); // Show "Learn More" link
+                viewLessLink.classList.add('hidden'); // Hide "View Less" link
+
+                // Scroll to the Learn More section
+                expandedDescription.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        }
