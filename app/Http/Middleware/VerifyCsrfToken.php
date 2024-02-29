c2ded545ba3682884129b4049b5a0737c3b5aee6
unknown <himlee@gmail.com>
2024-02-29 13:17:19 +0000

    Added Weak Session ID Vulnerability lesson and practice functionality; working properly

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'weak_session.low',
+        'weak_session.medium',
+        'weak_session.high',
     ];
+    
 }
