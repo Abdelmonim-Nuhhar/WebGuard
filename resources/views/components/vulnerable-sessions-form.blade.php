fffe56a407e656d26ff5892fde3fa79d95c7f2a1
unknown <himlee@gmail.com>
2024-03-03 23:07:58 +0000

    Add other vulnerability levels for weak session ID vulnerability and implement file content toggle button

--- resources/views/components/vulnerable-sessions-form.blade.php
+++ resources/views/components/vulnerable-sessions-form.blade.php
@@ -9,10 +9,14 @@
 </head>
 <body>
     <div class="p-4 m-2 rounded-lg shadow-lg bg-gray-100 border-2 border-blue-900 font-mono">
-        <h1 class="text-xl font-bold mb-2 text-gray-600">Weak Session IDs</h1>
 
+    <div class="bg-gray-200 p-3 mb-4 rounded-2xl hover:bg-gray-300 border-blue-700 border-2">
+        <h1 class="text-xl font-bold mb-2 text-gray-800">Weak Session IDs</h1>
+        <hr class="my-2 border-blue-800">
+        <h1 class="text-xl font-bold mb-2 text-gray-800">Practise Sesction</h1>
+    </div>
         
-        <div>
+        <div class="bg-gray-300 p-6 mb-8 rounded-2xl hover:bg-gray-200">
             <h2 class="text-xl font-semibold mb-2 text-blue-600">Low Level</h2>
             <form method="POST" action="{{ route('weak_session.low') }}">
                 @csrf
@@ -28,14 +32,14 @@
                     <input type="text" id="found_session_id" name="found_session_id" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                 </div>
                 <div class="mt-4">
-                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         Check Session ID
                     </button>
                 </div>
             </form>
         </div>
 
-        <div>
+        <div class="bg-gray-300 p-6 mb-8 rounded-2xl hover:bg-gray-200">
             <h2 class="text-xl font-semibold mb-2 text-yellow-600">Medium Level</h2>
             <form method="POST" action="{{ route('weak_session.medium') }}">
                 @csrf 
@@ -51,18 +55,19 @@
                 <input type="text" id="found_session_id_medium" name="found_session_id_medium" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
             <div class="mt-4">
-                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                <button type="submit" class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
                     Check Session ID
                 </button>
             </div>
         </form>
         </div>
 
+        <div class="bg-gray-300 p-6 mb-8 rounded-2xl hover:bg-gray-200">
         <div>
             <h2 class="text-xl font-semibold mb-2 text-red-600">High Level</h2>
             <form method="POST" action="{{ route('weak_session.high') }}">
                 @csrf 
-                <button type="submit" class="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                <button type="submit" class="bg-red-300 hover:bg-red-700 text-gray-700 font-bold py-2 px-4 rounded">
                         Generate Session ID (High)
                     </button>
                 </form>
@@ -70,23 +75,24 @@
 
         <form method="POST" action="{{ route('weak_session.high') }}">
             @csrf 
-            <div class="mt-4">
+            <div class="mt-4 ">
                 <label for="found_session_id_high" class="block text-gray-700 text-sm font-bold mb-2">Please enter your guess for the next session ID:</label>
                 <input type="text" id="found_session_id_high" name="found_session_id_high" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
             <div class="mt-4">
-                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                <button type="submit" class="bg-red-300 hover:bg-red-700 text-gray-700 font-bold py-2 px-4 rounded">
                     Check Session ID
                 </button>
             </div>
         </form>
+        </div>
 
-        <div>
+        <div class="bg-gray-300 p-6 mb-8 rounded-2xl hover:bg-gray-200">
             <h2 class="text-xl font-semibold mb-2 text-red-800">impossible Level</h2>
             <form method="POST" action="{{ route('weak_session.impossible') }}">
                 @csrf 
                 <button type="submit" class="bg-red-500 hover:bg-red-900 text-white font-bold py-2 px-4 rounded">
-                        Generate Session ID (High)
+                        Generate Session ID (Impossible)
                     </button>
                 </form>
 
@@ -97,7 +103,7 @@
                 <input type="text" id="found_session_id_impossible" name="found_session_id_impossible" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
             <div class="mt-4">
-                <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                <button type="submit" class="bg-red-500 hover:bg-red-900 text-white font-bold py-2 px-4 rounded">
                     Check Session ID
                 </button>
             </div>
